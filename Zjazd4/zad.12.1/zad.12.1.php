90e899b76d31b807846771857f19251d144822f0
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2023-06-04 19:01:41 +0200

    Create zad.12.1.php

---
+++ Zjazd4/zad.12.1/zad.12.1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dane</title>
+</head>
+<body>
+<h1>Dane ogólne</h1>
+<form action="str2.php" method="POST">
+    <label>Numer karty:</label>
+    <input type="text" name="card_number"><br>
+
+    <label>Dane zamawiającego:</label>
+    <input type="text" name="customer_name"><br>
+
+    <label>Ilość osób:</label>
+    <input type="number" name="num_of_people"><br>
+
+    <input type="submit" value="Dalej">
+</form>
+</body>
+</html>
